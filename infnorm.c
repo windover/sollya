2953701e1c4ad35536cffe9445dea1f6e33c45aa
clauter <clauter@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2006-10-19 14:38:50 +0000

    Changed slightly accurateinfnorm constants.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@149 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- infnorm.c
+++ infnorm.c
@@ -3322,7 +3322,7 @@ int accurateInfnorm(mpfr_t result, node *func, rangetype range, chain *excludes,
   mpfr_init2(currDiameter, prec);
   mpfr_init2(stopDiameter, prec);
 
-  mpfr_div_2ui(stopDiameter,rangeDiameter,6,GMP_RNDD);
+  mpfr_div_2ui(stopDiameter,rangeDiameter,4,GMP_RNDD);
 
   okay = 0;
 
