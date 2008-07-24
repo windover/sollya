193eeeff97c78fb5e5ebd017a0e3803e9378da62
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2008-07-24 14:16:26 +0000

    update of sollya.h in order to allow more possibilities
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@659 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- sollya.h
+++ sollya.h
@@ -49,6 +49,7 @@ knowledge of the CeCILL-C license and that you accept its terms.
 #ifndef SOLLYA_H
 #define SOLLYA_H
 
+#include <stdlib.h>
 #include <mpfr.h>
 #include <mpfi.h>
 #include <setjmp.h>
@@ -91,6 +92,47 @@ struct libraryProcedureStruct
   void *code;
   chain *signature;
 };
+
+
+#define VARIABLE 0
+#define CONSTANT 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define SQRT 6
+#define EXP 7
+#define LOG 8
+#define LOG_2 9
+#define LOG_10 10
+#define SIN 11
+#define COS 12
+#define TAN 13
+#define ASIN 14
+#define ACOS 15
+#define ATAN 16
+#define SINH 17
+#define COSH 18
+#define TANH 19
+#define ASINH 20
+#define ACOSH 21
+#define ATANH 22
+#define POW 23
+#define NEG 24
+#define ABS 25
+#define DOUBLE 26
+#define DOUBLEDOUBLE 27
+#define TRIPLEDOUBLE 28
+#define POLYNOMIAL 29
+#define ERF 30
+#define ERFC 31
+#define LOG_1P 32
+#define EXP_M1 33
+#define DOUBLEEXTENDED 34
+#define LIBRARYFUNCTION 35
+#define CEIL 36
+#define FLOOR 37
+#define PI_CONST 38
 
 typedef struct nodeStruct node;
 struct nodeStruct 
@@ -223,5 +265,7 @@ extern void sortChain(chain *c,  int (*f) (void *, void *));
 extern chain *makeIntPtrChainFromTo(int m, int n);
 extern void freeIntPtr(void *ptr);
 
+extern void *safeCalloc (size_t nmemb, size_t size);
+extern void *safeMalloc (size_t size);
 
 #endif
