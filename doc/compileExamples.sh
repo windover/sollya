01f0e13af60ed327f7ce3b8f39791721cac2db45
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2008-08-29 16:38:28 +0000

    Changed names of example files for the introduction. Updated compileExamples.sh
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@698 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- doc/compileExamples.sh
+++ doc/compileExamples.sh
@@ -5,30 +5,46 @@ tempfile="/tmp/hlp2tex_tempfile"
 
 
 
-exampleFile=$1
-target=`basename $exampleFile .txt`".tex"
+main() {
+  if [ $# -eq 0 ]
+  then liste=`/bin/ls introExample*.sollya`
+  else liste=$*
+  fi
 
-touch $target
-rm $target
-
-echo "\begin{center}\begin{minipage}{15cm}\begin{Verbatim}[frame=single]" > $target
-
-nLines=`cat $exampleFile | wc -l`
-i=1;
-count=0;
-total=0;
-
-while [ $i -le $nLines ]
+  for exampleFile in $liste
   do
-  echo -n "> " >> $target
-  cat $exampleFile | head -n $i | tail -n 1 | sed -n 's/\t/    /g;p' | sed -n 's/\(..............................................................................\)/\1\n/g;p' >> $target
-   echo "roundingwarnings=on!;" "`head -n $i $exampleFile`" | $sollyaBin > $tempfile
-   sed -i -n 's/^//;p' $tempfile
-   total=`cat $tempfile | wc -l`
-   count=`expr $total - $count`
-   tail -n $count $tempfile | sed -n 's/\t/    /g;p' | sed -n 's/\(................................................................................\)/\1\n/g;p' >> $target
-   count=$total
-   i=`expr $i + 1`
-done
-echo "\end{Verbatim}" >> $target
-echo "\end{minipage}\end{center}" >> $target
+    echo "Processing file "$exampleFile
+    target=`basename $exampleFile .sollya`".tex"
+
+    touch $target
+    rm $target
+
+    echo "\begin{center}\begin{minipage}{15cm}\begin{Verbatim}[frame=single]" > $target
+
+    nLines=`cat $exampleFile | wc -l`
+    i=1;
+    count=0;
+    total=0;
+    
+    while [ $i -le $nLines ]
+    do
+      if ! cat $exampleFile | head -n $i | tail -n 1 | grep "/\*" > /dev/null
+      then
+        echo -n "> " >> $target
+	cat $exampleFile | head -n $i | tail -n 1 | sed -n 's/\t/    /g;p' | sed -n 's/\(..............................................................................\)/\1\n/g;p' >> $target
+	echo "roundingwarnings=on!;" "`head -n $i $exampleFile`" | $sollyaBin > $tempfile
+	sed -i -n 's/^//;p' $tempfile
+	total=`cat $tempfile | wc -l`
+	count=`expr $total - $count`
+	tail -n $count $tempfile | sed -n 's/\t/    /g;p' | sed -n 's/\(................................................................................\)/\1\n/g;p' >> $target
+	count=$total
+      fi
+
+    i=`expr $i + 1`
+    done
+    echo "\end{Verbatim}" >> $target
+    echo "\end{minipage}\end{center}" >> $target
+  done
+}
+
+main $*
