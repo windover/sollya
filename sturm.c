55ff632cd7623690c85bfd8ed9c0ba48d023d350
jmio17 <jmio17@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2008-09-12 11:54:13 +0000

    modified to find all distinct roots in the closed interval [a,b], a<=b.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@716 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- sturm.c
+++ sturm.c
@@ -33,7 +33,7 @@ int getNrRoots(int *res, void **args) {
   node **coefficients;
   mpfr_t *fpCoefficients;
   mpq_t  *qCoefficients;
-  
+  int r;
   int *fpCoeffRoundAutomatically;
   mpfr_t tempValue;
   node *tempTree;
@@ -74,9 +74,12 @@ int getNrRoots(int *res, void **args) {
       if (tempTree->nodeType != CONSTANT) {
     printMessage(1,"Warning: the %dth coefficient of the polynomial to implement is neither a floating point\n",i);
     printMessage(1,"constant nor is able to be evaluated without rounding to a floating point constant.\n");
-    printMessage(1,"Will evaluate it in round-to-nearest with the current precision (%d bits) before rounding to\n",prec);
-    printMessage(1,"the target precision. A double rounding issue may occur.\n");
-    evaluateFaithful(fpCoefficients[i], tempTree, tempValue, prec);
+    printMessage(1,"Will evaluate it faithfully with the current precision (%d bits) \n",prec);
+    r=evaluateFaithful(fpCoefficients[i], tempTree, tempValue, prec);
+    if (!r){
+      mpfr_set_ui(fpCoefficients[i],0,GMP_RNDN);
+      printMessage(1,"Warning: Forced the coefficient %d to 0 \n",i);
+      }
     fpCoeffRoundAutomatically[i] = 1;
       } 
       else {
@@ -226,15 +229,16 @@ int sturm_mpq(int *n, mpq_t *p, int p_degree, mpfi_t x){
   mpq_t *evalResA;
   mpq_t *evalResB;
   mpq_t *s0, *s1;
-
+  mpq_t evalRes;
   int s0_degree, s1_degree;
   mpfr_t a,b;
   mpq_t aq,bq;
-  int i,evalNr,prec;
+  int i,na,nb,prec,nrRoots;
   int varSignB,varSignA;
 
   prec=getToolPrecision();
-  evalNr=0;  
+  na=0; nb=0;  
+  nrRoots=0;
   mpfr_init2(a,prec);
   mpfr_init2(b,prec);
   mpfi_get_left(a,x);
@@ -264,27 +268,51 @@ int sturm_mpq(int *n, mpq_t *p, int p_degree, mpfi_t x){
   }
         
   polynomialDeriv_mpq(&dp, &dp_degree, p, p_degree);
-  
-  mpq_init(evalResA[evalNr]);
-  mpq_init(evalResB[evalNr]);
+  mpq_init(evalRes);
     
-  polynomialEval_mpq( &evalResA[evalNr], aq, s0, s0_degree);
-  polynomialEval_mpq( &evalResB[evalNr], bq, s0, s0_degree);
-  
-  evalNr++;
+  polynomialEval_mpq( &evalRes, aq, s0, s0_degree);
+  if (mpq_cmp_ui(evalRes,0,1)!=0){
+    mpq_init(evalResA[na]);
+    mpq_set(evalResA[na],evalRes);
+    na++;
+  }
+  else nrRoots++; /*if the left extremity is a root we count it here
+                    since the number of sign changes gives the nr of distinct 
+                    roots between a and b, a<b*/  
+  polynomialEval_mpq( &evalRes, bq, s0, s0_degree);
+  if (mpq_cmp_ui(evalRes,0,1)!=0){
+    mpq_init(evalResB[nb]);
+    mpq_set(evalResB[nb],evalRes);
+    nb++;
+  }
   s1=dp;
   s1_degree=dp_degree;
   if (s0_degree>0){
-    mpq_init(evalResA[evalNr]);
+
+    polynomialEval_mpq( &evalRes, aq, dp, dp_degree);
+    if (mpq_cmp_ui(evalRes,0,1)!=0){
+      mpq_init(evalResA[na]);
+      mpq_set(evalResA[na],evalRes);
+      na++;
+    }
+  
+    polynomialEval_mpq( &evalRes, bq, dp, dp_degree);
+    if (mpq_cmp_ui(evalRes,0,1)!=0){
+      mpq_init(evalResB[nb]);
+      mpq_set(evalResB[nb],evalRes);
+      nb++;
+    }
+
+    /*mpq_init(evalResA[evalNr]);
     mpq_init(evalResB[evalNr]);
     polynomialEval_mpq(&evalResA[evalNr], aq, dp, dp_degree);
     polynomialEval_mpq( &evalResB[evalNr], bq, dp, dp_degree);
-    evalNr++;  
+    evalNr++;*/  
   }
   
   while (s1_degree!=0){
-    mpq_init(evalResA[evalNr]);
-    mpq_init(evalResB[evalNr]);
+   // mpq_init(evalResA[evalNr]);
+    //mpq_init(evalResB[evalNr]);
           
     polynomialDivide_mpq(quotient, &quotient_degree, rest, &rest_degree, s0, s0_degree, s1, s1_degree) ;
     
@@ -301,28 +329,42 @@ int sturm_mpq(int *n, mpq_t *p, int p_degree, mpfi_t x){
     for (i=0; i<=s1_degree; i++)
       mpq_neg(s1[i],s1[i]);
     
-    polynomialEval_mpq( &evalResA[evalNr], aq, s1, s1_degree);
-    polynomialEval_mpq(  &evalResB[evalNr], bq, s1, s1_degree);
-    evalNr++;
+   // polynomialEval_mpq( &evalResA[evalNr], aq, s1, s1_degree);
+   // polynomialEval_mpq(  &evalResB[evalNr], bq, s1, s1_degree);
+   // evalNr++;
+    polynomialEval_mpq( &evalRes, aq, s1, s1_degree);
+    if (mpq_cmp_ui(evalRes,0,1)!=0){
+      mpq_init(evalResA[na]);
+      mpq_set(evalResA[na],evalRes);
+      na++;
+    }
+  
+    polynomialEval_mpq( &evalRes, bq, s1, s1_degree);
+    if (mpq_cmp_ui(evalRes,0,1)!=0){
+      mpq_init(evalResB[nb]);
+      mpq_set(evalResB[nb],evalRes);
+      nb++;
+    }
   }
   
   varSignA=0;
-  for (i=1; i<evalNr; i++){
+  for (i=1; i<na; i++){
     if ((mpq_cmp_ui(evalResA[i-1],0,1) * mpq_cmp_ui(evalResA[i],0,1))<0) 
       varSignA++;
-    if (((mpq_cmp_ui(evalResA[i-1],0,1) * mpq_cmp_ui(evalResA[i],0,1))==0)&&  ((mpq_cmp_ui(evalResA[i-1],0,1)<0) || (mpq_cmp_ui(evalResA[i],0,1)<0)))  
-      varSignA++;
+    
   } 
   varSignB=0;
-  for (i=1; i<evalNr; i++){
+  for (i=1; i<nb; i++){
     if ((mpq_cmp_si(evalResB[i-1],0,1) * mpq_cmp_si(evalResB[i],0,1))<0) 
       varSignB++;
-    if (((mpq_cmp_ui(evalResB[i-1],0,1) * mpq_cmp_ui(evalResB[i],0,1))==0)&&  ((mpq_cmp_ui(evalResB[i-1],0,1)<0) || (mpq_cmp_ui(evalResB[i],0,1)<0)))  
-      varSignB++;
   }
-  //printf("\nA variations: = %d\n", varSignA);
-  //printf("\nB variations: = %d\n", varSignB);
-  *n=(((varSignA-varSignB)>0)?(varSignA-varSignB):(varSignB-varSignA) );
+ // printf("\nA variations: = %d\n", varSignA);
+ // printf("\nB variations: = %d\n", varSignB);
+  *n=(((varSignA-varSignB)>0)?(varSignA-varSignB+nrRoots):(varSignB-varSignA+nrRoots) );
+
+  
+  
+
 return 1;
 }
 
