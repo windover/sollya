a27931992d024b83c3b640f811a777e4211c0f37
clauter <clauter@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2008-09-15 10:49:05 +0000

    Changed option syntax and added --help option
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@718 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- general.c
+++ general.c
@@ -834,16 +834,27 @@ int general(int argc, char *argv[]) {
   int parseAbort, executeAbort;
   int i;
 
+  for (i=1;i<argc;i++) {
+    if (strcmp(argv[i],"--nocolor") == 0) noColor = 1;
+    if (strcmp(argv[i],"--noprompt") == 0) eliminatePromptBackup = 1;
+    if (strcmp(argv[i],"--oldrlwrapcompatible") == 0) oldrlwrapcompatible = 1;
+    if (strcmp(argv[i],"--help") == 0) {
+      printf("This is %s.\n\n",PACKAGE_STRING);
+      printf("Usage: %s [options]\n\nPossible options are:\n",argv[0]);
+      printf("--nocolor : do not color the output using ANSI escape sequences\n");
+      printf("--noprompt : do not print a prompt symbol\n");
+      printf("--oldrlwrapcompatible : acheive some compatibilty with old rlwrap versions by emitting wrong ANSI sequences (deprecated)\n");
+      printf("--help : print this help text\n");
+      printf("\nFor help on %s commands type \"help;\" on the %s prompt\n",PACKAGE_NAME,PACKAGE_NAME);
+      printf("More documentation on %s is available on the %s website.\nFor bug reports send an email to %s.\n",PACKAGE_NAME,PACKAGE_NAME,PACKAGE_BUGREPORT);
+      return 1;
+    }
+  }
+
   yylex_init(&scanner);
 
   if (tcgetattr(0,&termAttr) == -1) {
     eliminatePromptBackup = 1;
-  }
-
-  for (i=1;i<argc;i++) {
-    if (strcmp(argv[i],"-nocolor") == 0) noColor = 1;
-    if (strcmp(argv[i],"-noprompt") == 0) eliminatePromptBackup = 1;
-    if (strcmp(argv[i],"-oldrlwrapcompatible") == 0) oldrlwrapcompatible = 1;
   }
   
   initSignalHandler();
