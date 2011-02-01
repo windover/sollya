a0b2e37749e21e5a6dfa04098459d71945cd1744
clauter <clauter@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2011-02-01 17:10:27 +0000

    Added seealso references for the commands that have two names
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@1272 af37bc7e-59aa-4313-bbaf-7d19c0032f32

---
+++ doc/TD.php
@@ -0,0 +1,11 @@
+<a name="td"></a> 
+<div class="divName"> 
+<h2 class="name">Name:</h2> <?php linkTo("command","td","TD");?> 
+<span class="smallDescription">short form for <?php linkTo("command","tripledouble","tripledouble");?> 
+</span> 
+</div> 
+<div class="divExamples"> 
+</div> 
+<div class="divSeeAlso"> 
+<span class="category">See also: </span><?php linkTo("command","tripledouble","tripledouble");?> 
+</div> 
