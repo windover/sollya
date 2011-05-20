e5f3db6e3d7b07867e32470e80bd32f0082d6ed4
clauter <clauter@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2011-05-20 08:56:19 +0000

    Recompiled the documentation
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@1381 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- doc/www/bashevaluate.php
+++ doc/www/bashevaluate.php
@@ -39,7 +39,7 @@ such.
 <div class="divExample"> 
 <h2 class="category">Example 1: </h2> 
 &nbsp;&nbsp;&nbsp;&gt; bashevaluate("LANG=C date");<br> 
-&nbsp;&nbsp;&nbsp;Wed May 18 11:14:12 CEST 2011<br> 
+&nbsp;&nbsp;&nbsp;Fri May 20 10:42:50 CEST 2011<br> 
 </div> 
 <div class="divExample"> 
 <h2 class="category">Example 2: </h2> 
