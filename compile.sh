d9489db17da7138c389040b37b7f22cd8a3351ec
clauter <clauter@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2011-06-16 08:03:25 +0000

    Synchronize metalibm and sollya
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@1403 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- compile.sh
+++ compile.sh
@@ -5,8 +5,8 @@ cd $1
 echo "#include \"expansion.h\"" > impl.c
 echo " " >> impl.c
 cat $beforedir/$2 | sed -e 's/void/static inline void/g;' | sed -e "s/static inline void $4(/void $4(/g;" >> impl.c
-gcc -Winline -std=c99 -O2 -D$3 -DPOLYNOMIALNAME=$4 -fPIC  -c impl.c 
-gcc -Winline -std=c99 -D$3 -DPOLYNOMIALNAME=$4 -fPIC  -c expansion.c 
+gcc -Winline -finline-limit=1200 -std=c99 -O2 -D$3 -DPOLYNOMIALNAME=$4 -fPIC  -c impl.c 
+gcc -Winline -finline-limit=1200 -std=c99 -D$3 -DPOLYNOMIALNAME=$4 -fPIC  -c expansion.c 
 gcc -std=c99 -shared -o $5 impl.o expansion.o -lgmp -lmpfr 
 #rm impl.c
 rm impl.o
