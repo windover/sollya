ec4a577df69ecf9f92905e9c79580e9b60e12580
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2013-01-25 11:54:06 +0000

    * Du ménage : création d'une section misc pour accueillir des fichier reliés à Sollya et à son développement, mais qui n'ont pas à être distribués avec le tronc. * Suppression de la branche experimentalChebModels.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@1909 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- taylorModel.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef AUTODIFF_H
-#define AUTODIFF_H
-//void taylor_model(mpfi_t* rem_bound, mpfi_t* res_array, node *f, mpfi_t x, int n);
-tModel* createEmptytModel(int n,  mpfi_t x);
-void taylor_model(tModel *t, node *f, mpfi_t x, int n);
-#endif
