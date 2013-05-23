f967cece895700524333ca7e220ed2d0f6cac2c8
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2013-05-23 13:25:50 +0000

    * Updated remez documentation with the new 6th argument.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@2056 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- doc/www/remez.php
+++ doc/www/remez.php
@@ -12,8 +12,8 @@
 </div> 
 <div class="divUsage"> 
 <h2 class="category">Usage: </h2> 
-<span class="commandline"><?php linkTo("command","remez","remez");?>(<span class="arg">f</span>, <span class="arg">n</span>, <span class="arg">range</span>, <span class="arg">w</span>, <span class="arg">quality</span>) : (<span class="type">function</span>, <span class="type">integer</span>, <span class="type">range</span>, <span class="type">function</span>, <span class="type">constant</span>) -&gt; <span class="type">function</span></span> 
-<span class="commandline"><?php linkTo("command","remez","remez");?>(<span class="arg">f</span>, <span class="arg">L</span>, <span class="arg">range</span>, <span class="arg">w</span>, <span class="arg">quality</span>) : (<span class="type">function</span>, <span class="type">list</span>, <span class="type">range</span>, <span class="type">function</span>, <span class="type">constant</span>) -&gt; <span class="type">function</span></span> 
+<span class="commandline"><?php linkTo("command","remez","remez");?>(<span class="arg">f</span>, <span class="arg">n</span>, <span class="arg">range</span>, <span class="arg">w</span>, <span class="arg">quality</span>, <span class="arg">bounds</span>) : (<span class="type">function</span>, <span class="type">integer</span>, <span class="type">range</span>, <span class="type">function</span>, <span class="type">constant</span>, <span class="type">range</span>) -&gt; <span class="type">function</span></span> 
+<span class="commandline"><?php linkTo("command","remez","remez");?>(<span class="arg">f</span>, <span class="arg">L</span>, <span class="arg">range</span>, <span class="arg">w</span>, <span class="arg">quality</span>, <span class="arg">bounds</span>) : (<span class="type">function</span>, <span class="type">list</span>, <span class="type">range</span>, <span class="type">function</span>, <span class="type">constant</span>, <span class="type">range</span>) -&gt; <span class="type">function</span></span> 
  
 </div> 
 <div class="divParameters"> 
@@ -25,6 +25,7 @@
 <li><span class="arg">range</span> is the interval where the function must be approximated</li> 
 <li><span class="arg">w</span> (optional) is a weight function. Default is 1.</li> 
 <li><span class="arg">quality</span> (optional) is a parameter that controls the quality of the returned polynomial <span class="arg">p</span>, with respect to the exact minimax p*. Default is 1e-5.</li> 
+<li><span class="arg">bounds</span> (optional) is a parameter that allows the user to make the algorithm stop earlier, whenever a given accuracy is reached or a given accuracy is proved unreachable. Default is [0, +Inf].</li> 
 </ul> 
 </div> 
 <div class="divDescription"> 
@@ -43,8 +44,8 @@ relative error.
 greater than n. 
 If <span class="arg">L</span> is given and is a list of integers, p is searched as a linear 
 combination of monomials X^k where k belongs to <span class="arg">L</span>. 
-In the case when <span class="arg">L</span> is a list of integers, it may contain ellipses but cannot 
-be end-elliptic. 
+In the case when <span class="arg">L</span> is a list of integers, it may contain ellipses but 
+cannot be end-elliptic. 
 If <span class="arg">L</span> is given and is a list of functions g_k, p is searched as a 
 linear combination of the g_k. In that case <span class="arg">L</span> cannot contain ellipses. 
 It is the user responsability to check that the g_k are linearly independent 
@@ -55,9 +56,28 @@ found the minimax, though the returned p is not optimal.
 </li><li>The polynomial is obtained by a convergent iteration called Remez' 
 algorithm (and an extension of this algorithm, due to Stiefel). 
 The algorithm computes a sequence p1, ..., pk, ... 
-such that ek = ||<span class="arg">pk*w-f</span>|| converges towards 
+such that ek = ||pk*w-f|| converges towards 
 the optimal value e. The algorithm is stopped when the relative error 
 between ek and e is less than <span class="arg">quality</span>. 
+</li><li>The optional argument <span class="arg">bounds</span> is an interval [satisfying_err, target_err] 
+with the following behavior:<ul> 
+  <li> if, during the algorithm, we manage to prove that target_err is 
+    unreachable, we stop the algorithm returning the last computed 
+    polynomial. 
+  </li><li> if, during the algorithm, we obtain a polynomial with an error smaller 
+    than satisfying_err, we stop the algorithm returning that polynomial. 
+  </li><li> otherwise we loop until we find an optimal polynomial with the required 
+    quality, as usual.</li></ul> 
+Examples of use:<br> 
+  [0, +Inf] (compute the optimal polynomial with the required quality)<br> 
+  [target_err] (stops as soon as a polynomial achieving target_err is 
+                obtained or as soon as such a polynomial is proved not to 
+                exist).<br> 
+  [0, target_err] (finds the optimal polynomial, but provided that its error 
+                   is smaller than target_err).<br> 
+  [satisfying_err, +Inf] (stops as soon as a polynomial achieving 
+                          satisfying_err is obtained. If such a polynomial 
+                          does not exist, returns the optimal polynomial). 
 </ul> 
 </div> 
 <div class="divExamples"> 
@@ -98,6 +118,27 @@ between ek and e is less than <span class="arg">quality</span>.
 &nbsp;&nbsp;&nbsp;9.8841323829271038137685646777951687620288462194745e-8<br> 
 &nbsp;&nbsp;&nbsp;&gt; dirtyinfnorm(p2 - g, [-1/16;1/16]);<br> 
 &nbsp;&nbsp;&nbsp;2.54337800593461418356437401152248866818783932027105e-9<br> 
+</div> 
+<div class="divExample"> 
+<h2 class="category">Example 5: </h2> 
+&nbsp;&nbsp;&nbsp;&gt; f = sin(x);<br> 
+&nbsp;&nbsp;&nbsp;&gt; I = [-3b-5;-1b-1074];<br> 
+&nbsp;&nbsp;&nbsp;&gt; time(popt = remez(1, [|1, 3, 4, 5, 7, 8, 9|], I, 1/f));<br> 
+&nbsp;&nbsp;&nbsp;1.25521300000000000018571255644417306029936298727989<br> 
+&nbsp;&nbsp;&nbsp;&gt; time(p1 = remez(1, [|1, 3, 4, 5, 7, 8, 9|], I, 1/f, default, [0, 1b-73]));<br> 
+&nbsp;&nbsp;&nbsp;0.79467800000000000004880817972008344440837390720844<br> 
+&nbsp;&nbsp;&nbsp;&gt; time(p2 = remez(1, [|1, 3, 4, 5, 7, 8, 9|], I, 1/f, default, [3b-72, +@Inf@]));<br> 
+&nbsp;&nbsp;&nbsp;0.9891289999999999999792665850151252016075886785984<br> 
+&nbsp;&nbsp;&nbsp;&gt; dirtyinfnorm(popt/f-1, I);<br> 
+&nbsp;&nbsp;&nbsp;2.06750931454112835098093903810531156576504665659064e-22<br> 
+&nbsp;&nbsp;&nbsp;&gt; dirtyinfnorm(p1/f-1, I);<br> 
+&nbsp;&nbsp;&nbsp;2.49711266837493110470637913808914046704452778960875e-22<br> 
+&nbsp;&nbsp;&nbsp;&gt; dirtyinfnorm(p2/f-1, I);<br> 
+&nbsp;&nbsp;&nbsp;5.4567247553615435246376977231253834265248756996947e-22<br> 
+&nbsp;&nbsp;&nbsp;&gt; 1b-73;<br> 
+&nbsp;&nbsp;&nbsp;1.05879118406787542383540312584955245256423950195312e-22<br> 
+&nbsp;&nbsp;&nbsp;&gt; 3b-72;<br> 
+&nbsp;&nbsp;&nbsp;6.3527471044072525430124187550973147153854370117187e-22<br> 
 </div> 
 </div> 
 <div class="divSeeAlso"> 
