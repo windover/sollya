6688dac8d79af16c26f5e45df72a472d420e0333
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2013-05-27 13:06:15 +0000

    * Updated doc of chebyshevform
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@2072 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- doc/www/chebyshevform.php
+++ doc/www/chebyshevform.php
@@ -1,13 +1,13 @@
 <a name="chebyshevform"></a> 
 <div class="divName"> 
 <h2 class="name">Name:</h2> <?php linkTo("command","chebyshevform","chebyshevform");?> 
-<span class="smallDescription">computes a rigorous polynomial approximation  
+<span class="smallDescription">computes a rigorous polynomial approximation 
 </span> 
 </div> 
 <div class="divLibraryName"> 
 <h2 class="libraryname">Library name:</h2> 
-<span class="commandline type">sollya_obj_t sollya_lib_chebyshevform(sollya_obj_t, sollya_obj_t, </span> 
-<span class="commandline type">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sollya_obj_t); </span> 
+<span class="commandline type">sollya_obj_t sollya_lib_chebyshevform(sollya_obj_t, sollya_obj_t,</span> 
+<span class="commandline type">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sollya_obj_t);</span> 
 </div> 
 <div class="divUsage"> 
 <h2 class="category">Usage: </h2> 
@@ -24,48 +24,47 @@
 </div> 
 <div class="divDescription"> 
 <h2 class="category">Description: </h2><ul> 
-<li><?php linkTo("command","chebyshevform","chebyshevform");?> computes an approximation polynomial and an interval error bound 
-for a given function f.  
+<li>WARNING: <?php linkTo("command","chebyshevform","chebyshevform");?> is a certified command, not difficult to use but not 
+completely straightforward to use either. In order to be sure to use it 
+correctly, the reader is invited to carefully read this documentation 
+entirely. 
+</li><li><?php linkTo("command","chebyshevform","chebyshevform");?> computes an approximation polynomial and an interval error 
+bound for a given function f. 
 <br> 
 More precisely, it returns a list L=[p, coeffErrors, Delta, chebCoeffs] 
-where: 
-<ul> 
-<li> p is an approximation polynomial of degree n  
-which is roughly speaking a numerical Chebyshev interpolation polynomial  
-of f over I represented in monomial basis. 
-</li><li> coeffsErrors is a list of n+1 intervals. Each  
-interval coeffsErrors[i] contains an enclosure of all the  
-errors accumulated when computing the i-th coefficient of p. 
-</li><li> Delta is an interval that provides a bound for the  
-approximation error between p and f. See details below. 
-</li><li> chebCoeffs is a list of  
-n+1 intervals. These are the interval coefficients obtained for  
-the representation of interpolation polynomial considered in Chebyshev  
-basis. See details below. 
-</li></ul> 
-</li><li>The polynomial p and the bound Delta are obtained 
-using Chebyshev Models principles. 
+where:<ul> 
+<li> p is an approximation polynomial of degree n which is roughly speaking a 
+numerical Chebyshev interpolation polynomial of f over I represented in 
+monomial basis. 
+</li><li> coeffsErrors is a list of n+1 intervals. Each interval coeffsErrors[i] 
+contains an enclosure of all the errors accumulated when computing the i-th 
+coefficient of p. 
+</li><li> Delta is an interval that provides a bound for the approximation error 
+between p and f. See details below. 
+</li><li> chebCoeffs is a list of n+1 intervals. These are the interval 
+coefficients obtained for the representation of interpolation polynomial 
+considered in Chebyshev basis. See details below.</li></ul> 
+</li><li>The polynomial p and the bound Delta are obtained using Chebyshev Models 
+principles. 
 </li><li>More formally, the mathematical property ensured by the algorithm may be 
 stated as follows: 
 <br> 
-Using monomial basis representation given by p: there exist  
-(small) values eps[i] in coeffsErrors[i]  
-such that: 
-for all x in I, there exists delta in Delta such 
-that f(x) - p(x) = sum{i=0...n} eps[i]*x^i + delta. 
+Using monomial basis representation given by p: there exist (small) values 
+eps[i] in coeffsErrors[i] such that: 
+for all x in I, there exists delta in Delta such that 
+                f(x) - p(x) = sum{i=0...n} eps[i]*x^i + delta. 
 <br> 
-Using Chebyshev basis representation given by chebCoeffs:  
-there exist values alpha[i] in chebCoeffs[i]  
-such that: 
-for all x in I, there exists delta in Delta such 
-that f(x) - sum{i=0...n} alfa[i]*T_i(y) =delta,  
-where [a,b]=I, y=(2*x-b-a)/(b-a) and  T_i(y)  
-is the i-th Chebyshev polynomial over [-1,1]. 
+Using Chebyshev basis representation given by chebCoeffs: there exist values 
+alpha[i] in chebCoeffs[i] such that: 
+for all x in I, there exists delta in Delta such that 
+                f(x) - sum{i=0...n} alpha[i]*T_i(y) = delta, 
+where [a,b]=I, y=(2*x-b-a)/(b-a) and T_i(y) is the i-th Chebyshev polynomial 
+over [-1,1]. 
 </li><li>The algorithm does not guarantee that by increasing the degree of the 
-approximation, the remainder bound will become smaller. Moreover, it may  
-even become larger due to the dependecy phenomenon present with interval 
+approximation, the remainder bound will become smaller. Moreover, it may even 
+become larger due to the dependecy phenomenon present with interval 
 arithmetic. In order to reduce this phenomenon, a possible solution is to 
-split the definition domain I into several smaller intervals.  
+split the definition domain I into several smaller intervals. 
 </ul> 
 </div> 
 <div class="divExamples"> 
