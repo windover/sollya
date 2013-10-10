c132b42957125fe66c8034b3c43f8c44d39ce68e
schevill <schevill@af37bc7e-59aa-4313-bbaf-7d19c0032f32>
2013-10-10 13:01:20 +0000

    * Patched a nasty bug in Newton: whenever the iterator of Newton's algorithm evaluates to NaN, the next point of the algorithm used to be NaN leading to an infinite loop. This bug is here for a very long time, but seems to have been undiscovered until now, because it is barely impossible to produce. We incidentally discovered it two revisions ago, because of deep modifications that we made in the code used to simplify expressions. The problem has been detected by check012 which felt into an infinite loop. These modifications have been further improved at previous revision, hiding again the bug in Newton. I am unfortunately not able to produce a non-regression test in the current state of the repository.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/sollya/trunk@2175 af37bc7e-59aa-4313-bbaf-7d19c0032f32

--- remez.c
+++ remez.c
@@ -633,7 +633,7 @@ void findZero(mpfr_t res, node *f, node *f_diff, mpfr_t a, mpfr_t b, int sgnfa, 
       r = evaluateFaithfulWithCutOffFast(xNew, iterator, NULL, x, zero_mpfr, prec);
       if(r==0) mpfr_set_d(xNew,0,GMP_RNDN);
 
-      if( (mpfr_cmp(u,xNew)>0) || (mpfr_cmp(xNew,v)>0) || ((!mpfr_number_p(xNew)) && ((r==1) || (r==4) || (r==5) || (r==6) || (r==7))) ) {
+      if( (mpfr_cmp(u,xNew)>0) || (mpfr_cmp(xNew,v)>0) || (!mpfr_number_p(xNew)) ) {
 	printMessage(5, SOLLYA_MSG_NEWTON_PERFORMING_BISECTION_STEP, "Information (Newton's algorithm): performing a bisection step\n");
 	mpfr_add(xNew,u,v,GMP_RNDN);
 	mpfr_div_2ui(xNew, xNew, 1, GMP_RNDN);
